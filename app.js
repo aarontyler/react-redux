c30de89a9d1b5976d038bf0e11377046914bdbf0
Aaron Tyler <aaron.tyler@konicaminolta.com.au>
2017-04-08 21:26:33 +1000

    Video 1

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('listenting to this joint on port 3000');
+});
